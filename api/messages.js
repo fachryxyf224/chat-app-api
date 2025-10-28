425de20b98fdbbe8c7630d0cd87d5793080ff0dd
Fachryxyf <fachryfauzansyafei@gmail.com>
2025-10-28 07:10:20 +0700

    Second push: fix API

--- api/messages.js
+++ api/messages.js
@@ -1,6 +1,8 @@
 // api/messages.js
 import { Pool } from 'pg';
 
+console.log("DATABASE_URL from Vercel:", process.env.DATABASE_URL);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
